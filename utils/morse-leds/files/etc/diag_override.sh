b392dfc815bda6486cbec0576216dd010de842f6
Evan Benn <evan.benn@morsemicro.com>
2024-08-21 04:40:42 +0000

    morse-leds: APP-3302: (PR #750): Update script to use PWM status LED
    The status LED (LED 1) is now a PWM RGB LED. Update the script to
    explicitly control the colors of the LEDs. Decrease the intensity of the
    blue LED a little.
    
    Approved-by: James Haggerty
    Approved-by: Sophronia Koilpillai

--- utils/morse-leds/files/etc/diag_override.sh
+++ utils/morse-leds/files/etc/diag_override.sh
@@ -3,9 +3,8 @@
 # Copyright 2024 Morse Micro
 
 # This is a rewritten version of /etc/diag.sh from base-files which
-# adds more possible states. It also simplifies things by disabling
-# all LEDs on every call (so we don't have to test what to disable)
-# and passing by argument rather than via setting status_led.
+# adds more possible states and handles RGB leds. It also simplifies
+# things by passing by argument rather than via setting status_led.
 
 . /lib/functions.sh
 . /lib/functions/leds.sh
@@ -28,19 +27,26 @@ if [ -z "$_mm_mode" ]; then
 	esac
 fi
 
-boot="$(get_dt_led boot)"
-failsafe="$(get_dt_led failsafe)"
-running="$(get_dt_led running)"
-runningsta="$(get_dt_led runningsta)"
-upgrade="$(get_dt_led upgrade)"
-dpp="$(get_dt_led dpp)"
+# halow and wifi leds are partly controlled by this script and partly by
+# /etc/init.d/led (which is configured by uci, which is configured by
+# /etc/board.d/). The status led is controlled at boot by device tree
+# configuration and the driver, and then only by this script.
+halow="$(get_dt_led halow)"
+wifi="$(get_dt_led wifi)"
+status="$(get_dt_led status)"
 
-disable_all_leds() {
-	led_off "$boot"
-	led_off "$failsafe"
-	led_off "$running"
-	led_off "$upgrade"
-	led_off "$dpp"
+red="255 0 0"
+green="0 255 0"
+blue="0 0 127"
+yellow="255 255 0"
+cyan="0 255 127"
+magenta="255 0 127"
+
+led_set_color() {
+	# input is <led> <R G B>, The assumption here is that
+	# /sys/class/leds/$1/multi_index is always "red green blue". And that
+	# /sys/class/leds/$1/max_brightness is always 255
+	led_set_attr "$1" multi_intensity "$2"
 }
 
 led_blink_slow() {
@@ -59,58 +65,62 @@ led_blink_veryfast() {
 	led_timer "$1" 50 50
 }
 
-set_state() {
-	disable_all_leds
+set_leds_normal() {
+	# This sets the color of all leds and the blink pattern on status only.
+	# The blink pattern of the other leds is controlled by /etc/init.d/led
+	led_set_color "$halow" "$magenta"
+	led_set_color "$wifi" "$green"
+	led_set_color "$status" "$green"
+	if [ "$_mm_mode" = sta ]; then
+		led_set_color "$status" "$cyan"
+	fi
+	led_on "$status"
+}
 
+# The transitions are:
+# failsafe -> preinit_regular -> done
+# preinit -> done
+# done -> upgrade
+# done -> ap_change -> sta_change -> factory_reset
+# done -> dpp_started -> dpp_failed -> done
+set_state() {
 	case "$1" in
-	preinit)
-		led_blink_fast "$running"
-		if [ "$_mm_mode" = sta ]; then
-			led_blink_fast "$runningsta"
-		fi
+	preinit|preinit_regular)
+		set_leds_normal
+		led_blink_fast "$status"
 		;;
 	failsafe)
-		led_blink_veryfast "$failsafe"
-		;;
-	preinit_regular)
-		led_blink_fast "$running"
-		if [ "$_mm_mode" = sta ]; then
-			led_blink_fast "$runningsta"
-		fi
+		led_set_color "$status" "$red"
+		led_blink_veryfast "$status"
 		;;
 	upgrade)
-		led_blink "$upgrade"
+		led_set_color "$status" "$blue"
+		led_blink "$status"
 		;;
 	dpp_started)
-		led_blink_slow "$dpp"
+		led_set_color "$status" "$red"
+		led_blink_slow "$status"
 		;;
 	dpp_failed)
-		led_blink_fast "$dpp"
+		led_set_color "$status" "$red"
+		led_blink_fast "$status"
 		;;
 	factory_reset)
-		# On Morse RGB LED devices, this usually produces a yellow
-		# (which is the same as the uboot colour).
-		led_blink "$failsafe"
-		led_blink "$running"
+		led_set_color "$status" "$yellow"
+		led_blink "$status"
 		;;
 	ap_change)
 		# Changing to AP mode.
-		led_blink "$running"
+		led_set_color "$status" "$green"
+		led_blink "$status"
 		;;
 	sta_change)
 		# Changing to STA mode.
-		led_blink_fast "$running"
-		led_blink_fast "$runningsta"
+		led_set_color "$status" "$cyan"
+		led_blink_fast "$status"
 		;;
 	done)
-		# Restore the boot LEDs default trigger since we might
-		# have finished messing with it.
-		status_led_restore_trigger boot
-
-		led_on "$running"
-		if [ "$_mm_mode" = sta ]; then
-			led_on "$runningsta"
-		fi
+		set_leds_normal
 		;;
 	esac
 }
