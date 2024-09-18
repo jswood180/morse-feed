4f5896414ca18d896bf0042dd313f03042b72410
Morse Micro <info@morsemicro.com>
2024-09-18 11:48:58 +1000

    morse-leds: add Artini LED support
    Artini has additional LEDs and utilises full RGB on the system LED
    to indicate different device states. Add handling to read the device
    mode from persistent storage and indicate the correct LED colour
    accordingly.

--- utils/morse-leds/files/etc/diag_override.sh
+++ utils/morse-leds/files/etc/diag_override.sh
@@ -10,6 +10,24 @@
 . /lib/functions.sh
 . /lib/functions/leds.sh
 
+# This lets us have a different basic LED colour depending
+# on the mode of the device.
+_mm_mode="$(persistent_vars_storage.sh READ mm_mode 2> /dev/null)"
+if [ -z "$_mm_mode" ]; then
+	# Sadly, fw_*.config files are populated by uci-defaults, and when we're
+	# early in the boot process we don't have it. But we really want to
+	# show the mode colour early in the boot process to avoid confusion.
+	# Board_name is also not available, so...
+	board_name="$(strings /proc/device-tree/compatible | head -1)"
+	case "$board_name" in
+	morse,artini)
+		echo '/dev/mtd1 0x0 0x1000 0x1000' > /tmp/artini_preinit_fw_sys.config
+		_mm_mode="$(fw_printenv -n -c /tmp/artini_preinit_fw_sys.config mm_mode 2> /dev/null)"
+		rm /tmp/artini_preinit_fw_sys.config
+		;;
+	esac
+fi
+
 boot="$(get_dt_led boot)"
 failsafe="$(get_dt_led failsafe)"
 running="$(get_dt_led running)"
@@ -47,12 +65,18 @@ set_state() {
 	case "$1" in
 	preinit)
 		led_blink_fast "$running"
+		if [ "$_mm_mode" = sta ]; then
+			led_blink_fast "$runningsta"
+		fi
 		;;
 	failsafe)
 		led_blink_veryfast "$failsafe"
 		;;
 	preinit_regular)
 		led_blink_fast "$running"
+		if [ "$_mm_mode" = sta ]; then
+			led_blink_fast "$runningsta"
+		fi
 		;;
 	upgrade)
 		led_blink "$upgrade"
@@ -84,6 +108,9 @@ set_state() {
 		status_led_restore_trigger boot
 
 		led_on "$running"
+		if [ "$_mm_mode" = sta ]; then
+			led_on "$runningsta"
+		fi
 		;;
 	esac
 }
